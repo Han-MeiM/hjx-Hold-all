03960af923a1be912746fcb9de59364400a054f7
Han-MeiM <969437631@qq.com>
2017-11-26 11:47:45 +0800

    阿里大于配置文件

---
+++ application/extra/Alicode.php
@@ -0,0 +1,6 @@
+<?php
+return [
+    'AccessKeyId'              => '', // 访问密钥，在阿里云的密钥管理页面创建
+    'TemplateCode'             => '', // 短信模板ID
+    'AccessSecret'             => '', // 访问密钥，在阿里云的密钥管理页面创建
+];
