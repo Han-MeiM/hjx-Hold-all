ea4fe06c7a67f62aa3360f15da22ba3591c26bba
Han-MeiM <969437631@qq.com>
2017-12-01 20:55:36 +0800

    邮件配置文件

---
+++ application/extra/Email.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * 发送邮件相关配置
+ */
+return [
+    'host' => 'smtp.163.com', // 所使用的邮箱smtp地址
+    'port' => 25, // 端口号
+    'username' => '', // 发送邮件的邮箱
+    'password' => '', // 授权密码
+    'charset' => 'utf-8', // 编码格式
+];
