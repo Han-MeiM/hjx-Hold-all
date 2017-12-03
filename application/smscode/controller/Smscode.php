940180b66d407f4050e899cdbcb419ced995873e
Han-MeiM <969437631@qq.com>
2017-12-03 22:06:23 +0800

    添加容联云短信发送方法

--- application/smscode/controller/Smscode.php
+++ application/smscode/controller/Smscode.php
@@ -18,4 +18,21 @@ class Weixinpay extends Controller
             echo '发送成功';
         }
     }
+
+    /**
+     * 容联云发送短信
+     */
+    public function rly_smscode(){
+        $phone = 18000000000;
+        $code = mt_rand(1000,9999);
+        // 实例化类
+        $sendcode = new \mobilecode\SendCode;
+        $tempid = 1;
+        // $phone = 接收的手机号，$code = 发送的验证码，$tempid = 模版id
+        $result = $sendcode->send($phone,$code,$tempid);
+        if ($result['Code'] == 'OK')
+        {
+            echo '发送成功';
+        }
+    }
 }
