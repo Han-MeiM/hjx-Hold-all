95e495686f5a248771fb2945ca9a1ebb7e15d2a7
Han-MeiM <969437631@qq.com>
2017-12-06 21:07:38 +0800

    添加发送邮箱实例

--- application/email/controller/Email.php
+++ application/email/controller/Email.php
@@ -1,13 +1,27 @@
 <?php
 namespace app\email\controller;
 use think\Controller;
+use think\Request;
 class Email extends Controller
 {
     public function send()
     {
-        $result = \phpmailer\Email::send('test@test.com','测试标题123','测试内容1223','韩佳鑫');
-        if ($result == 1){
-            return '发送哦邮件成功';
+        if (request()->isPost()) {
+            $data = input('post.');
+            // 使用定界符拼接发送的内容
+            $str= <<<EOT
+姓名：{$data['name']} <br>
+地址：{$data['address']} <br>
+手机号：{$data['phone']}
+EOT;
+            echo $str;
+            // 发送邮箱;邮件标题;邮件内容;发件人
+            $result = \phpmailer\Email::send($data['email'], '测试标题123', $str, '韩佳鑫');
+            if ($result == 1) {
+                return '发送邮件成功!';
+            }
+        }else{
+            return $this->fetch();
         }
     }
 }
