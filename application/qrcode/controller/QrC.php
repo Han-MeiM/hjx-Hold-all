b23416a55f7ccfd757662696265ad4d5efd808fb
Mr.狼先生 <969437631@qq.com>
2017-12-13 22:45:07 +0800

    给二维码添加中间logo

--- application/qrcode/controller/QrC.php
+++ application/qrcode/controller/QrC.php
@@ -3,14 +3,15 @@ namespace app\qrcode\controller;
 use think\Controller;
 class QrC extends Controller
 {
-	public function index()
-	{
-		return $this->fetch();
-	}
+    public function index()
+    {
+        return $this->fetch();
+    }
 
-	public function creQr()
-	{
-        echo qrcode('https://www.hanjiaxin.com','韩佳鑫的个人blog');
+    public function creQr()
+    {
+        // 内容,下方注释,中间logo地址,logo大小,二维码大小
+        echo qrcode('https://www.hanjiaxin.com','韩佳鑫的个人blog','icon.jpg',50);
         exit;
-	}
+    }
 }
