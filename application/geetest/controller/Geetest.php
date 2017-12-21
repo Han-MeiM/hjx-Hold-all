d2280d4da2297ca88e43953766954c481b5c2230
Han-MeiM <969437631@qq.com>
2017-12-21 22:31:30 +0800

    修改获取用户IP地址

--- application/geetest/controller/Geetest.php
+++ application/geetest/controller/Geetest.php
@@ -18,7 +18,7 @@ class Geetest extends Controller
         $data = array(
             "user_id" => session('user_id'), # 网站用户id
             "client_type" => "web", #web:电脑上的浏览器；h5:手机上的浏览器，包括移动应用内完全内置的web_view；native：通过原生SDK植入APP应用的方式
-            "ip_address" => "127.0.0.1" # 请在此处传输用户请求验证时所携带的IP
+            "ip_address" => request()->ip() # 请在此处传输用户请求验证时所携带的IP
         );
 
         $status = $GtSdk->pre_process($data, 1);
@@ -42,7 +42,7 @@ class Geetest extends Controller
         $data = array(
             "user_id" => session('user_id'), # 网站用户id
             "client_type" => "web", #web:电脑上的浏览器；h5:手机上的浏览器，包括移动应用内完全内置的web_view；native：通过原生SDK植入APP应用的方式
-            "ip_address" => "127.0.0.1" # 请在此处传输用户请求验证时所携带的IP
+            "ip_address" => request()->ip() # 请在此处传输用户请求验证时所携带的IP
         );
 
         // 获取session中存储的极验官网服务器状态
