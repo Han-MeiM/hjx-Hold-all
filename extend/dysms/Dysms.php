c5a7874a543ac6cc4d42b95958f6df44d2be2d24
Han-MeiM <969437631@qq.com>
2018-01-01 23:40:11 +0800

    　清除不用的use

--- extend/dysms/Dysms.php
+++ extend/dysms/Dysms.php
@@ -2,11 +2,8 @@
 namespace alicode;
 
 error_reporting(E_ALL);
-ini_set('display_errors', '1');
 // 定义时区
 ini_set('date.timezone','Asia/Shanghai');
-use think\Cache;
-use think\Db;
 
 class Dysms {
     /**
