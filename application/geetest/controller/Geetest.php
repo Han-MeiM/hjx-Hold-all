ec5aeffa5ee3c5ba2259be659f8110b4a98c21df
Han-MeiM <969437631@qq.com>
2018-01-07 23:39:53 +0800

    规范写法

--- application/geetest/controller/Geetest.php
+++ application/geetest/controller/Geetest.php
@@ -1,6 +1,9 @@
 <?php
 namespace app\geetest\controller;
+
 use think\Controller;
+use geetest\geetestlib;
+
 class Geetest extends Controller
 {
     public function index()
@@ -13,7 +16,7 @@ class Geetest extends Controller
      */
     public function starCaptcha()
     {
-        $GtSdk = new \geetest\geetestlib(config('geetest.CAPTCHA_ID'), config('geetest.PRIVATE_KEY'));
+        $GtSdk = new geetestlib(config('geetest.CAPTCHA_ID'), config('geetest.PRIVATE_KEY'));
 
         $data = array(
             "user_id" => session('user_id'), # 网站用户id
@@ -23,8 +26,8 @@ class Geetest extends Controller
 
         $status = $GtSdk->pre_process($data, 1);
         // 记录极验官网服务器状态
-        session('gtserver',$status);
-        session('user_id',$data['user_id']);
+        session('gtserver', $status);
+        session('user_id', $data['user_id']);
         return json($GtSdk->get_response());
     }
 
@@ -36,9 +39,9 @@ class Geetest extends Controller
      * @param $geetest_seccode
      * @return string
      */
-    public function verifyLogin($geetest_challenge,$geetest_validate,$geetest_seccode)
+    public function verifyLogin($geetest_challenge, $geetest_validate, $geetest_seccode)
     {
-        $GtSdk = new \geetest\geetestlib(config('geetest.CAPTCHA_ID'), config('geetest.PRIVATE_KEY'));
+        $GtSdk = new geetestlib(config('geetest.CAPTCHA_ID'), config('geetest.PRIVATE_KEY'));
         $data = array(
             "user_id" => session('user_id'), # 网站用户id
             "client_type" => "web", #web:电脑上的浏览器；h5:手机上的浏览器，包括移动应用内完全内置的web_view；native：通过原生SDK植入APP应用的方式
