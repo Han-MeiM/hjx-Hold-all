4a96dd8d4f3c28be2f95e8df0a7fb47fa5658a63
han-meim <969437631@qq.com>
2018-04-11 22:27:23 +0800

    添加测试预检缓存的第三个请求地址

--- application/cors/controller/Cors.php
+++ application/cors/controller/Cors.php
@@ -21,4 +21,15 @@ class Cors extends Controller
 
         echo '恭喜跨域成功!';
     }
+
+    public function isOther()
+    {
+        header("Access-Control-Allow-Origin: *");
+        header("Access-Control-Allow-Credentials: true");
+        header("Access-Control-Allow-Headers: X-Custom-Header");
+        header("Access-Control-Allow-Methods: GET,POST,PUT,PATCH,DELETE");
+        header("Access-Control-Max-Age: 30");
+
+        echo "恭喜缓存预检成功";
+    }
 }
